97707e2d4573ad5cb904874d57beece998c6854a
karthi <karthirajendran12003@gmail.com>
2022-11-17 12:21:30 +0530

    model

--- model/domain.js
+++ model/domain.js
@@ -1,13 +1,10 @@
-
 const mongoose = require('mongoose');
 const { Schema } = mongoose
-
 
 const domainSchema = new Schema({
     user:[{type: Schema.Types.ObjectId,ref: "User",}],
     domain_name:String,
     image:String
 });
-
 
 module.exports = mongoose.model("Domain", domainSchema);
