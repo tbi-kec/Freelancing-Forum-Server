f502a4fd7e19225834d81bff8a9924a5b5f90302
karthi <karthirajendran12003@gmail.com>
2022-12-25 18:43:02 +0530

    forgotten-password

--- controllers/forgotten_password_controller.js
+++ controllers/forgotten_password_controller.js
@@ -7,11 +7,13 @@ const crypto = require("crypto");
 const { resetpassword_sendEmail } = require('./mail')
 
 exports.change_password_request = async (req, res) => {
+  const {kongu_email}=req.body
+  console.log(req.body)
   try {
-    const schema = Joi.object({ kongu_email: Joi.string().email().required() });
-    const { error } = schema.validate(req.body);
-    if (error) return res.status(400).json(error.details[0].message);
-    const user = await User.findOne({ kongu_email: req.body.kongu_email });
+    // const schema = Joi.object({ kongu_email: Joi.string().email().required() });
+    // const { error } = schema.validate(req.body);
+    // if (error) return res.status(400).json(error.details[0].message);
+    const user = await User.findOne({ kongu_email });
     if (!user)
       return res.status(400).json("User With Given Email Doesn't Exist");
     let token = await Token.findOne({ userId: user._id });
@@ -21,13 +23,14 @@ exports.change_password_request = async (req, res) => {
         token: crypto.randomBytes(32).toString("hex"),
       }).save();
     }
-    const link = `http://localhost:3000/forgotton-password/${user._id}/${token.token}`;
+    const link = `http://localhost:3000/forgotten-password/${user._id}/${token.token}`;
     await resetpassword_sendEmail(user.kongu_email, "Your Password reset request is accepted", link);
     res.status(200).json("Password Reset Link Sent To Your Email Account");
 
   } catch (error) {
-    res.status(500).json(error)
     console.log(error);
+    res.status(500).json(error)
+    
   }
 
 }
@@ -51,10 +54,12 @@ exports.change_password = async (req, res) => {
       await token.remove();
       res.status(200).json("Password Reset successfully");
     } catch (error) {
+      console.log(error.message)
       res.status(500).json("Internal Server Error");
     }
   }
   else {
+     console.log()
     res.status(500).json("Password and confirm Password Not Match!");
   }
 
