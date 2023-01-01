9683b82e14b60fb060fe582090e09247699abb95
LOGESH-B <logeshbommannan@gmail.com>
2023-01-01 12:29:28 +0530

    progress/route

--- routes/project_routes.js
+++ routes/project_routes.js
@@ -2,7 +2,7 @@ const express=require('express')
 const router = express.Router();
 
 
-const {getallproject,newProject,editProject,deleteProject,project_request,project_request_status,project_developer_request, project_developer_request_rejected} = require('../controllers/project_controller')
+const {getallproject,updateProgress,newProject,editProject,deleteProject,project_request,project_request_status,project_developer_request, project_developer_request_rejected} = require('../controllers/project_controller')
 
 router.get('/all',getallproject)
 router.post('/',newProject)
@@ -16,5 +16,8 @@ router.post('/provider/request/status',project_request_status)
 //developer
 router.post('/developer/request',project_developer_request)
 router.post('/developer/request/rejected',project_developer_request_rejected)
+
+//progress
+router.post('/update/progress',updateProgress)
 
 module.exports=router;
